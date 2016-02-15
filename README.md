47f350634d33e94e418c81050cc2d9af57345ae5
Louis-Rémi Babé <lrbabe@gmail.com>
2016-02-15 16:32:36 +0100

    Add Readme

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # gulp-shelter
-Cozy place for your shell tasks
+A cozy place for your shell tasks
+
+## Npm Scripts as a build tool, or Gulp tasks?
+
+Gulp-shelter brings the best of both worlds and makes everyone in the team happy.
+
+## Installing
+
+Gulp is the only hard dependency. But gulp-shelter will make your build scripts much simpler when written with template strings (**only compatible with Gulp v3.9.0+**).
+
+`npm install --save-dev gulp-shelter gulp@"^3.9.0" babel-core babel-register babel-preset-es2015 gulp-cli`
+
+Setup Babel to **use es2015**, if you haven't already:
+
+̀`echo '{ "presets": ["es2015"] }' > .babelrc`
+
+And finally name or rename your gulpfile to **`gulpfile.babel.js`**.
+
+## Using
+
+```js
+const gulp = require('gulp');
+const shelter = require('gulp-shelter')(gulp);
+
+// project config
+const project = 'projectName';
+const main = `src/main.js`;
+const dest = `dist/${project}.js`;
+
+// commands and fragments
+const browserifyOpts = `
+	--standalone ${project}
+	--transform [ babelify --presets [ es2015 react ] ]
+	--debug
+`;
+const browserify = `browserify ${main} ${browserifyOpts}`;
+const exorcist = `exorcist ${dest}.map > ${dest}`;
+const watchify = `watchify ${main} ${browserifyOpts} -o ${dest}`;
+const browsersync = `browser-sync start --server --files "${dest}, index.html"`;
+
+shelter({
+	build: {
+		dsc: 'generate standalone ${project} lib and source-map',
+		cmd: `${browserify} | ${exorcist}`
+	},
+	serve: {
+		dsc: 'Open index.html in the browser and live-reload on changes',
+		cmd: `${watchify} & ${browsersync}`
+	}
+});
+```
+
+Note that it's not required that your command consist of `{ dsc: …, cmd: … }` objects.
+A string will do as well, but adding a description will make your tasks discoverable and explicit using `gulp --tasks`;
+
+### No global Gulp required
+
+One of the advantages of npm scripts is that they do not require any other command line tools to be installed.
+You can get the same benefit by adding the following lines to your `package.json`:
+```
+…
+"scripts": {
+	"gulp": "gulp",
+    "help": "gulp --tasks",
+	…
+},
+…
+```
+
+Now tasks can be discovered by running **`npm run help`**, and individual tasks can be run with **`npm run gulp -- <task name>`**.
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -1,35 +1,19 @@
-var path = require('path'),
-	childProcess = require('child_process'),
-	_ = require('lodash');
+var path = require('path');
+var childProcess = require('child_process');
+var _ = require('lodash');
 
-var gulp,
-	templates = {},
-	config = {};
+var gulp;
 
 function taskBuilder( name, _task ) {
-	var task = {};
+	var task = (
+		typeof _task === 'string' ?
+			{ cmd: _task } :
+			task = _task
+	);
 
-
-	if ( typeof _task.cmd === 'function' ) {
-		task.fn = _task.cmd;
-
-	} else if ( typeof _task.cmd === 'string' ) {
-		task.cmd = _task.cmd;
-
-	} else if ( Array.isArray( _task.cmd ) ) {
-		task.cmd = _task.cmd.join(' ');
-	}
-
-	if ( !task.fn && task.cmd ) {
-		templates[name] = _.template( task.cmd );
-
-		// make the command available to templates
-		// (interpolate template recursively when it's used)
-		Object.defineProperty(config, name, {
-			get: function() {
-				return templates[name]( config );
-			}
-		});
+	if ( task.cmd ) {
+		// prepend backslash to newlines
+		task.cmd = task.cmd.replace(/\n/g, ' \\\n');
 
 		task.fn = function(done) {
 			var opts = _.defaults( task.opts || {}, {
@@ -37,36 +21,34 @@ function taskBuilder( name, _task ) {
 					env: process.env,
 					stdio: 'inherit',
 					usePowerShell: false
-				}),
-				spawnOpts = {
+				});
+			var spawnOpts = {
 					cwd: opts.cwd,
 					env: opts.env,
 					stdio: opts.stdio
-				},
-				// use getter defined above to retrieve the interpolated command
-				command = config[ name ],
-				oldPath = opts.env.PATH,
-				subshell;
+				};
+			var oldPath = opts.env.PATH;
+			var subshell;
 
 			// Include node_modules/.bin on the path
 			spawnOpts.env.PATH =
-				path.join( process.cwd(), 'node_modules', '.bin') +
+				path.join( process.cwd(), 'node_modules', '.bin' ) +
 				path.delimiter + oldPath;
 
 			subshell = process.platform.lastIndexOf('win') === 0 ?
 				( opts.usePowerShell ?
 					childProcess.spawn( 'powershell.exe',
-						[ '-NonInteractive', '-NoLogo', '-Command', command ],
+						[ '-NonInteractive', '-NoLogo', '-Command', task.cmd ],
 						spawnOpts ) :
 					childProcess.spawn( 'cmd.exe',
-						[ '/c', command ], spawnOpts )
+						[ '/c', task.cmd ], spawnOpts )
 				) :
-				childProcess.spawn( 'sh', [ '-c', command ], spawnOpts );
+				childProcess.spawn( 'sh', [ '-c', task.cmd ], spawnOpts );
 
 			subshell.once('close', function(code) {
 				if ( code !== 0 ) {
 					var err = new Error(
-						'Command `' + command + '` exited with code ' + code
+						'Command `' + task.cmd + '` exited with code ' + code
 					);
 
 					err.status = code;
@@ -80,39 +62,28 @@ function taskBuilder( name, _task ) {
 
 	if ( task.fn ) {
 		gulp.task( name, task.fn );
-		if ( _task.dsc ) {
-			task.fn.description = _.template( _task.dsc )( config );
+		if ( 'dsc' in _task ) {
+			task.fn.description = _task.dsc;
+		}
+		if ( 'flg' in _task ) {
+			task.fn.flags = _task.flg;
 		}
 	}
 }
 
-function shelter( _config ) {
-	if ( !_config ) {
+function shelter( tasks ) {
+	if ( !tasks ) {
 		return;
 	}
 
-	_.forEach(_config, function( prop, key ) {
-		if ( Array.isArray( prop ) ) {
-			prop = prop.join(' ');
-		}
-
-		if ( typeof prop === 'string' ) {
-			templates[key] = _.template( prop );
-			Object.defineProperty( config, key, {
-				get: function() {
-					return templates[key]( config );
-				}
-			});
-
-		} else if ( typeof prop === 'object' ) {
-			taskBuilder( key, prop );
-		}
+	_.forEach(tasks, function( prop, key ) {
+		taskBuilder( key, prop );
 	});
 }
 
 shelter.task = taskBuilder;
 
-// module must be required with `require('gulp-shelter')(gulp, run)`
+// module must be required with `require('gulp-shelter')(gulp)`
 // to be able to register tasks
 module.exports = function( _gulp ) {
 	gulp = _gulp;
